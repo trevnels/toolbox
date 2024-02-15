228add2e32ff60f135fe6ee4725e57e201fac0f2
trevnels <25140503+trevnels@users.noreply.github.com>
2024-02-15 16:42:06 -0500

    Fix xdg path oops

--- distrobox.fish
+++ distrobox.fish
@@ -2,7 +2,7 @@ alias hx="helix"
 alias ls="eza --icons"
 
 set -gx EDITOR "helix"
-set -gx XDG_CONFIG_HOME "$HOME/.cache-distrobox"
+set -gx XDG_CACHE_HOME "$HOME/.cache-distrobox"
 set -g fish_greeting
 
 starship init fish | source
